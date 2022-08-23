ef9c8e053ea067295bb3272e412035c6801e3f84
sketch <einfach2live@foxmail.com>
2022-08-23 22:52:07 +0800

    商品层级分类API

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/BaseAttrController.java
@@ -0,0 +1,28 @@
+package com.atguigu.gmall.product.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sketch
+ * @date 2022/8/23 22:27
+ * @description 平台属性相关API
+ */
+@RestController
+@RequestMapping("/admin/product")
+public class BaseAttrController {
+
+    // TODO: 2022/8/23
+    /**
+     * 1. 查询某个分类下的所有平台属性
+     */
+
+
+    /**
+     * 2. 保存、修改属性信息
+     */
+
+    /**
+     * 3. 根据平台属性id，获取这个属性的所有属性值
+     */
+}
--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryController.java
@@ -3,8 +3,10 @@ package com.atguigu.gmall.product.controller;
 import com.atguigu.gmall.common.result.Result;
 import com.atguigu.gmall.model.product.BaseCategory1;
 import com.atguigu.gmall.model.product.BaseCategory2;
+import com.atguigu.gmall.model.product.BaseCategory3;
 import com.atguigu.gmall.product.service.BaseCategory1Service;
 import com.atguigu.gmall.product.service.BaseCategory2Service;
+import com.atguigu.gmall.product.service.BaseCategory3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,7 @@ import java.util.List;
 /**
  * @author sketch
  * @date 2022/8/22 20:56
+ * @description 商品层级分类API
  */
 @RestController
 @RequestMapping("/admin/product")
@@ -25,6 +28,8 @@ public class CategoryController {
     BaseCategory1Service baseCategory1Service;
     @Autowired
     BaseCategory2Service baseCategory2Service;
+    @Autowired
+    BaseCategory3Service baseCategory3Service;
 
     /**
      * 1. 获取所有商品一级分类
@@ -39,9 +44,18 @@ public class CategoryController {
      * 2. 获取所有一级分类下的二级分类
      */
     @GetMapping("/getCategory2/{c1Id}")
-    public Result getCategory2(@PathVariable("c1Id")Long c1Id){
+    public Result getCategory2(@PathVariable("c1Id") Long c1Id) {
         //查询 父分类id是c1Id 的所有二级分类
         List<BaseCategory2> category2s = baseCategory2Service.getCategory1Child(c1Id);
         return Result.ok(category2s);
+    }
+
+    /**
+     * 3. 获取所有二级分类下的三级分类
+     */
+    @GetMapping("/getCategory3/{c2Id}")
+    public Result getCategory3(@PathVariable("c2Id") Long c2Id) {
+        List<BaseCategory3> list = baseCategory3Service.getCategory2Child(c2Id);
+        return Result.ok(list);
     }
 }
